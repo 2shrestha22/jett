c14e9c598c6b83067ab4f353cd6bbdfc69dba341
Sangam Shrestha <2shrestha22@gmail.com>
2025-08-20 07:35:36 +0545

    update multicast address and ports

--- lib/discovery/konst.dart
+++ lib/discovery/konst.dart
@@ -1,6 +1,6 @@
-const int kUdpPort = 53317;
-const String kAddress = '224.0.0.167';
-const int kTcpPort = 53318;
+const int kUdpPort = 37020;
+const String kAddress = '239.255.0.1';
+const int kTcpPort = 37021;
 
 // Constants for device discovery and communication
 const Duration cleanUpInterval = Duration(milliseconds: 1000);
