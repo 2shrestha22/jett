fbde14daa046882d10e615edfb449caf175cc564
Sangam Shrestha <2shrestha22@gmail.com>
2025-08-23 11:50:21 +0545

    make speed animation smooth

--- lib/screen/transfer_screen.dart
+++ lib/screen/transfer_screen.dart
@@ -7,6 +7,7 @@ import 'package:anysend/utils/data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:forui/forui.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/streams.dart';
 
 enum TransferType { send, receive }
@@ -58,7 +59,9 @@ class _TransferScreenState extends State<TransferScreen> {
             children: [
               Spacer(),
               StreamBuilder(
-                stream: widget.speedometerReadingStream,
+                stream: widget.speedometerReadingStream.sampleTime(
+                  Duration(milliseconds: 400),
+                ),
                 builder: (context, snapshot) {
                   final speed = (snapshot.data?.speedBps ?? 0) / (1024 * 1024);
 
@@ -105,7 +108,7 @@ class _TransferScreenState extends State<TransferScreen> {
                         stream: widget.fileNameStream,
                       ),
                       TransferState.completed => Text(
-                        'Transfer completed ${formatTransferRate(widget.speedometerReadingStream.value?.avgSpeedBps ?? 0)}',
+                        'Transfer finished, ${formatTransferRate(widget.speedometerReadingStream.value?.avgSpeedBps ?? 0)}',
                       ),
                       TransferState.failed => Text('Transfer failed'),
                     };
