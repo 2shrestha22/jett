2shrestha22/anysend

make file access compatible with picker, drag and share intent

lib/transfer/client.dart
@@ -5,19 +5,16 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:anysend/discovery/konst.dart';
-import 'package:anysend/model/file_info.dart';
+import 'package:anysend/model/resource.dart';
 import 'package:anysend/transfer/speedometer.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
-import 'package:mime/mime.dart';
 import 'package:rxdart/streams.dart';
 import 'package:rxdart/subjects.dart';
-import 'package:uri_content/uri_content.dart';
 
 class Client {
   // final _httpClient = HttpClient();
   final _speedometer = Speedometer();
-  final _uriContent = UriContent();
 
   ValueStream<SpeedometerReading?> get speedometerReadingsStream =>
       _speedometer.readingStream;
@@ -48,7 +45,7 @@ class Client {
   /// Uploads files to the specified IP address.
   ///
   /// You should only upload files after the transfer request is accepted.
-  Future<void> upload(List<FileInfo> files, String ipAddr) async {
+  Future<void> upload(List<Resource> resources, String ipAddr) async {
     // user already cancelled send, using reset()
     if (_abortTrigger == null) return;
 
@@ -66,38 +63,38 @@ class Client {
     // create a multipart request body stream
     // and add speedometer counting to each file stream
     final requestMultipart = http.MultipartRequest('POST', uri);
-    for (var file in files) {
-      // TODO: in linux and windows URI is null and should be handled
-      // differently using file path
-      final size =
-          await _uriContent.getContentLength(Uri.parse(file.uri!)) ?? 0;
-      totalFileSize += size;
-      final fileStream = _uriContent
-          .getContentStream(Uri.parse(file.uri!), bufferSize: 1024 * 256)
-          .transform(
-            StreamTransformer<Uint8List, Uint8List>.fromHandlers(
-              handleData: (data, sink) {
-                sink.add(data);
-                _fileNameSubject.add(file.name);
-                _speedometer.count(data.length);
-              },
-              handleError: (error, stack, sink) {
-                _speedometer.stop();
-                throw error;
-              },
-              handleDone: (sink) {
-                _speedometer.stop();
-                sink.close();
-              },
-            ),
-          );
+    for (var resource in resources) {
+      final contentLenght = await resource.length();
+
+      if (contentLenght == null) return;
+
+      totalFileSize += contentLenght;
+
+      final contentStream = resource.openRead();
+      final fileStream = contentStream.transform(
+        StreamTransformer<Uint8List, Uint8List>.fromHandlers(
+          handleData: (data, sink) {
+            sink.add(data);
+            _fileNameSubject.add(resource.name);
+            _speedometer.count(data.length);
+          },
+          handleError: (error, stack, sink) {
+            _speedometer.stop();
+            throw error;
+          },
+          handleDone: (sink) {
+            _speedometer.stop();
+            sink.close();
+          },
+        ),
+      );
       requestMultipart.files.add(
         http.MultipartFile(
           'files',
           fileStream,
-          size,
-          filename: file.name,
-          contentType: _getContentType(file.path),
+          contentLenght,
+          filename: resource.name,
+          contentType: _getContentType(resource.mimeType),
         ),
       );
     }
@@ -147,8 +144,7 @@ Future<String> _readResponseAsString(http.StreamedResponse response) {
   return completer.future;
 }
 
-MediaType? _getContentType(String? filePath) {
-  final mimeType = filePath != null ? lookupMimeType(filePath) : null;
+MediaType? _getContentType(String? mimeType) {
   final contentType = mimeType != null ? MediaType.parse(mimeType) : null;
 
   return contentType;

lib/widgets/desktop_picker_button.dart
@@ -1,14 +1,14 @@
-import 'package:anysend/model/file_info.dart';
+import 'package:anysend/model/resource.dart';
 import 'package:anysend/widgets/drop_region.dart';
 import 'package:fast_file_picker/fast_file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
 
 class DesktopPickerButton extends StatelessWidget {
-  final void Function(List<FileInfo> files) onFileAdd;
+  final void Function(List<Resource> resources) onResourceAdd;
 
   // it is called multiple times when files are drag and dropped
-  const DesktopPickerButton({super.key, required this.onFileAdd});
+  const DesktopPickerButton({super.key, required this.onResourceAdd});
 
   @override
   Widget build(BuildContext context) {
@@ -17,18 +17,21 @@ class DesktopPickerButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(32.0),
       child: FileDropRegion(
-        onFileAdd: (fileInfo) {
-          onFileAdd([fileInfo]);
+        onResourceAdd: (fileResource) {
+          onResourceAdd([fileResource]);
         },
         child: FButton.raw(
           onPress: () async {
             final result = await FastFilePicker.pickMultipleFiles();
 
             if (result != null && result.isNotEmpty) {
-              final fileInfoList = result
-                  .map((e) => FileInfo(name: e.name, path: e.path, uri: e.uri))
-                  .toList();
-              onFileAdd(fileInfoList);
+              final resourceList = result.map((e) {
+                if (e.uri != null) {
+                  return ContentResource(uri: e.uri!, name: e.name);
+                }
+                return FileResource(e.path!);
+              }).toList();
+              onResourceAdd(resourceList);
             }
           },
           child: Container(

lib/widgets/drop_region.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
-import 'package:anysend/model/file_info.dart';
+import 'package:anysend/model/resource.dart';
 import 'package:anysend/utils/io.dart';
 import 'package:flutter/material.dart';
 import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 
 class FileDropRegion extends StatelessWidget {
-  final void Function(FileInfo uri) onFileAdd;
+  final void Function(Resource resource) onResourceAdd;
   final Widget child;
 
   const FileDropRegion({
     super.key,
-    required this.onFileAdd,
+    required this.onResourceAdd,
     required this.child,
   });
 
@@ -51,14 +51,19 @@ class FileDropRegion extends StatelessWidget {
           if (dataReader.canProvide(Formats.fileUri)) {
             dataReader.getValue(Formats.fileUri, (value) async {
               if (value != null) {
-                final fileName = value.pathSegments.last;
-                // TODO: check if you can read file in windows using this path
-                // or need to ues toFilePath() method
                 final fileType = await FileSystemEntity.type(value.path);
-                if (fileType == FileSystemEntityType.file) {
-                  // TODO: support folder picking
-                  onFileAdd(FileInfo(name: fileName, uri: value.toString()));
-                }
+
+                // TODO: support folder picking
+                // note: for some file, filetype is not found in mac
+                // but it works after renaming the file in the mac,
+                // weird issue
+                if (fileType == FileSystemEntityType.directory) return;
+                onResourceAdd(
+                  ContentResource(
+                    uri: value.toString(),
+                    name: value.pathSegments.last,
+                  ),
+                );
               }
             });
           }

lib/widgets/file_view.dart
@@ -1,4 +1,4 @@
-import 'package:anysend/model/file_info.dart';
+import 'package:anysend/model/resource.dart';
 import 'package:anysend/utils/data.dart' show formatFileSize;
 import 'package:flutter/material.dart';
 import 'package:forui/assets.dart';
@@ -7,13 +7,13 @@ import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:path/path.dart' as p;
 
 class FileInfoTile extends StatefulWidget {
-  final FileInfo fileInfo;
+  final Resource resource;
   final int? fileSize;
   final VoidCallback onRemoveTap;
 
   const FileInfoTile({
     super.key,
-    required this.fileInfo,
+    required this.resource,
     this.fileSize,
     required this.onRemoveTap,
   });
@@ -30,7 +30,7 @@ class _FileInfoTileState extends State<FileInfoTile> {
   void initState() {
     super.initState();
     fileType = p
-        .extension(widget.fileInfo.name)
+        .extension(widget.resource.name)
         .replaceFirst('.', '')
         .toUpperCase();
 
@@ -74,7 +74,7 @@ class _FileInfoTileState extends State<FileInfoTile> {
   Widget build(BuildContext context) {
     return FTile(
       prefix: Icon(icon, color: Colors.blue),
-      title: Text(widget.fileInfo.name, overflow: TextOverflow.ellipsis),
+      title: Text(widget.resource.name, overflow: TextOverflow.ellipsis),
       subtitle: Row(
         children: [
           Text(fileType),

lib/widgets/mobile_picker_button.dart
@@ -1,11 +1,13 @@
-import 'package:anysend/model/file_info.dart';
+import 'dart:io';
+
+import 'package:anysend/model/resource.dart';
 import 'package:fast_file_picker/fast_file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/widgets/button.dart';
 import 'package:forui_assets/forui_assets.dart';
 
 class MobilePickerButton extends StatelessWidget {
-  final void Function(List<FileInfo> files) onPick;
+  final void Function(List<Resource> resources) onPick;
 
   const MobilePickerButton({super.key, required this.onPick});
 
@@ -17,10 +19,14 @@ class MobilePickerButton extends StatelessWidget {
         final result = await FastFilePicker.pickMultipleFiles();
 
         if (result != null && result.isNotEmpty) {
-          final fileInfoList = result
-              .map((e) => FileInfo(name: e.name, path: e.path, uri: e.uri))
-              .toList();
-          onPick(fileInfoList);
+          final resourceList = result.map((e) {
+            if (e.uri != null) {
+              return ContentResource(uri: e.uri!, name: e.name);
+            } else {
+              return FileResource(e.path!);
+            }
+          }).toList();
+          onPick(resourceList);
         }
       },
       prefix: Icon(FIcons.file),

2shrestha22/jett

lib/transfer/client.dart
@@ -5,19 +5,16 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:jett/discovery/konst.dart';
-import 'package:jett/model/file_info.dart';
+import 'package:jett/model/resource.dart';
 import 'package:jett/transfer/speedometer.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
-import 'package:mime/mime.dart';
 import 'package:rxdart/streams.dart';
 import 'package:rxdart/subjects.dart';
-import 'package:uri_content/uri_content.dart';
 
 class Client {
   // final _httpClient = HttpClient();
   final _speedometer = Speedometer();
-  final _uriContent = UriContent();
 
   ValueStream<SpeedometerReading?> get speedometerReadingsStream =>
       _speedometer.readingStream;
@@ -48,7 +45,7 @@ class Client {
   /// Uploads files to the specified IP address.
   ///
   /// You should only upload files after the transfer request is accepted.
-  Future<void> upload(List<FileInfo> files, String ipAddr) async {
+  Future<void> upload(List<Resource> resources, String ipAddr) async {
     // user already cancelled send, using reset()
     if (_abortTrigger == null) return;
 
@@ -66,38 +63,38 @@ class Client {
     // create a multipart request body stream
     // and add speedometer counting to each file stream
     final requestMultipart = http.MultipartRequest('POST', uri);
-    for (var file in files) {
-      // TODO: in linux and windows URI is null and should be handled
-      // differently using file path
-      final size =
-          await _uriContent.getContentLength(Uri.parse(file.uri!)) ?? 0;
-      totalFileSize += size;
-      final fileStream = _uriContent
-          .getContentStream(Uri.parse(file.uri!), bufferSize: 1024 * 256)
-          .transform(
-            StreamTransformer<Uint8List, Uint8List>.fromHandlers(
-              handleData: (data, sink) {
-                sink.add(data);
-                _fileNameSubject.add(file.name);
-                _speedometer.count(data.length);
-              },
-              handleError: (error, stack, sink) {
-                _speedometer.stop();
-                throw error;
-              },
-              handleDone: (sink) {
-                _speedometer.stop();
-                sink.close();
-              },
-            ),
-          );
+    for (var resource in resources) {
+      final contentLenght = await resource.length();
+
+      if (contentLenght == null) return;
+
+      totalFileSize += contentLenght;
+
+      final contentStream = resource.openRead();
+      final fileStream = contentStream.transform(
+        StreamTransformer<Uint8List, Uint8List>.fromHandlers(
+          handleData: (data, sink) {
+            sink.add(data);
+            _fileNameSubject.add(resource.name);
+            _speedometer.count(data.length);
+          },
+          handleError: (error, stack, sink) {
+            _speedometer.stop();
+            throw error;
+          },
+          handleDone: (sink) {
+            _speedometer.stop();
+            sink.close();
+          },
+        ),
+      );
       requestMultipart.files.add(
         http.MultipartFile(
           'files',
           fileStream,
-          size,
-          filename: file.name,
-          contentType: _getContentType(file.path),
+          contentLenght,
+          filename: resource.name,
+          contentType: _getContentType(resource.mimeType),
         ),
       );
     }
@@ -147,8 +144,7 @@ Future<String> _readResponseAsString(http.StreamedResponse response) {
   return completer.future;
 }
 
-MediaType? _getContentType(String? filePath) {
-  final mimeType = filePath != null ? lookupMimeType(filePath) : null;
+MediaType? _getContentType(String? mimeType) {
   final contentType = mimeType != null ? MediaType.parse(mimeType) : null;
 
   return contentType;

lib/widgets/desktop_picker_button.dart
@@ -1,14 +1,14 @@
-import 'package:jett/model/file_info.dart';
+import 'package:jett/model/resource.dart';
 import 'package:jett/widgets/drop_region.dart';
 import 'package:fast_file_picker/fast_file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
 
 class DesktopPickerButton extends StatelessWidget {
-  final void Function(List<FileInfo> files) onFileAdd;
+  final void Function(List<Resource> resources) onResourceAdd;
 
   // it is called multiple times when files are drag and dropped
-  const DesktopPickerButton({super.key, required this.onFileAdd});
+  const DesktopPickerButton({super.key, required this.onResourceAdd});
 
   @override
   Widget build(BuildContext context) {
@@ -17,18 +17,21 @@ class DesktopPickerButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(32.0),
       child: FileDropRegion(
-        onFileAdd: (fileInfo) {
-          onFileAdd([fileInfo]);
+        onResourceAdd: (fileResource) {
+          onResourceAdd([fileResource]);
         },
         child: FButton.raw(
           onPress: () async {
             final result = await FastFilePicker.pickMultipleFiles();
 
             if (result != null && result.isNotEmpty) {
-              final fileInfoList = result
-                  .map((e) => FileInfo(name: e.name, path: e.path, uri: e.uri))
-                  .toList();
-              onFileAdd(fileInfoList);
+              final resourceList = result.map((e) {
+                if (e.uri != null) {
+                  return ContentResource(uri: e.uri!, name: e.name);
+                }
+                return FileResource(e.path!);
+              }).toList();
+              onResourceAdd(resourceList);
             }
           },
           child: Container(

lib/widgets/drop_region.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
-import 'package:jett/model/file_info.dart';
+import 'package:jett/model/resource.dart';
 import 'package:jett/utils/io.dart';
 import 'package:flutter/material.dart';
 import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 
 class FileDropRegion extends StatelessWidget {
-  final void Function(FileInfo uri) onFileAdd;
+  final void Function(Resource resource) onResourceAdd;
   final Widget child;
 
   const FileDropRegion({
     super.key,
-    required this.onFileAdd,
+    required this.onResourceAdd,
     required this.child,
   });
 
@@ -51,14 +51,19 @@ class FileDropRegion extends StatelessWidget {
           if (dataReader.canProvide(Formats.fileUri)) {
             dataReader.getValue(Formats.fileUri, (value) async {
               if (value != null) {
-                final fileName = value.pathSegments.last;
-                // TODO: check if you can read file in windows using this path
-                // or need to ues toFilePath() method
                 final fileType = await FileSystemEntity.type(value.path);
-                if (fileType == FileSystemEntityType.file) {
-                  // TODO: support folder picking
-                  onFileAdd(FileInfo(name: fileName, uri: value.toString()));
-                }
+
+                // TODO: support folder picking
+                // note: for some file, filetype is not found in mac
+                // but it works after renaming the file in the mac,
+                // weird issue
+                if (fileType == FileSystemEntityType.directory) return;
+                onResourceAdd(
+                  ContentResource(
+                    uri: value.toString(),
+                    name: value.pathSegments.last,
+                  ),
+                );
               }
             });
           }

lib/widgets/file_view.dart
@@ -1,5 +1,5 @@
-import 'package:jett/model/file_info.dart';
 import 'package:jett/utils/data.dart' show formatFileSize;
+import 'package:jett/model/resource.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/assets.dart';
 import 'package:forui/widgets/tile.dart';
@@ -7,13 +7,13 @@ import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:path/path.dart' as p;
 
 class FileInfoTile extends StatefulWidget {
-  final FileInfo fileInfo;
+  final Resource resource;
   final int? fileSize;
   final VoidCallback onRemoveTap;
 
   const FileInfoTile({
     super.key,
-    required this.fileInfo,
+    required this.resource,
     this.fileSize,
     required this.onRemoveTap,
   });
@@ -30,7 +30,7 @@ class _FileInfoTileState extends State<FileInfoTile> {
   void initState() {
     super.initState();
     fileType = p
-        .extension(widget.fileInfo.name)
+        .extension(widget.resource.name)
         .replaceFirst('.', '')
         .toUpperCase();
 
@@ -74,7 +74,7 @@ class _FileInfoTileState extends State<FileInfoTile> {
   Widget build(BuildContext context) {
     return FTile(
       prefix: Icon(icon, color: Colors.blue),
-      title: Text(widget.fileInfo.name, overflow: TextOverflow.ellipsis),
+      title: Text(widget.resource.name, overflow: TextOverflow.ellipsis),
       subtitle: Row(
         children: [
           Text(fileType),

lib/widgets/mobile_picker_button.dart
@@ -1,11 +1,11 @@
-import 'package:jett/model/file_info.dart';
+import 'package:jett/model/resource.dart';
 import 'package:fast_file_picker/fast_file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/widgets/button.dart';
 import 'package:forui_assets/forui_assets.dart';
 
 class MobilePickerButton extends StatelessWidget {
-  final void Function(List<FileInfo> files) onPick;
+  final void Function(List<Resource> resources) onPick;
 
   const MobilePickerButton({super.key, required this.onPick});
 
@@ -17,10 +17,14 @@ class MobilePickerButton extends StatelessWidget {
         final result = await FastFilePicker.pickMultipleFiles();
 
         if (result != null && result.isNotEmpty) {
-          final fileInfoList = result
-              .map((e) => FileInfo(name: e.name, path: e.path, uri: e.uri))
-              .toList();
-          onPick(fileInfoList);
+          final resourceList = result.map((e) {
+            if (e.uri != null) {
+              return ContentResource(uri: e.uri!, name: e.name);
+            } else {
+              return FileResource(e.path!);
+            }
+          }).toList();
+          onPick(resourceList);
         }
       },
       prefix: Icon(FIcons.file),