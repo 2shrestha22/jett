d105af3b0eaaf02cac60ba83b1cb7b5bb779d5a6
Sangam Shrestha <2shrestha22@gmail.com>
2026-09-05 11:24:13 +0000

    Migrate iOS app to UIScene lifecycle
    - Conform AppDelegate to FlutterImplicitEngineDelegate and move plugin
      registration and Pigeon setup into didInitializeImplicitFlutterEngine,
      removing the window?.rootViewController access that is nil under UIScene
    - Add SceneDelegate subclassing FlutterSceneDelegate to handle jett://share
      URLs from the share extension, which UIScene no longer delivers to
      application(_:open:options:)
    - Add UIApplicationSceneManifest to Info.plist
    - Add SceneDelegate.swift to the Runner target

---
+++ ios/Runner/SceneDelegate.swift
@@ -0,0 +1,19 @@
+import Flutter
+import UIKit
+
+class SceneDelegate: FlutterSceneDelegate {
+    override func scene(
+        _ scene: UIScene,
+        openURLContexts URLContexts: Set<UIOpenURLContext>
+    ) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            super.scene(scene, openURLContexts: URLContexts)
+            return
+        }
+
+        let unhandledContexts = URLContexts.filter { !appDelegate.handleSharedURL($0.url) }
+        if !unhandledContexts.isEmpty {
+            super.scene(scene, openURLContexts: Set(unhandledContexts))
+        }
+    }
+}
